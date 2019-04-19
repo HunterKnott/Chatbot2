b34a3c7cd76cfc4043d914fa473a1d175efdfbb3
Hunter <hunterknott00@gmail.com>
2019-04-19 12:16:18 -0600

    Size change to ChatFrame

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,29 +1,27 @@
-//_____________________________________________________________
-//-------------------------------------------------------------
 package chat.view;
 
 import javax.swing.JFrame;
 import chat.controller.ChatController;
-//-------------------------------------------------------------
+
 public class ChatFrame extends JFrame
 {
 	private ChatController appController;
 	private ChatPanel appPanel;
-	//_____________________________________________________________
+	
 	public ChatFrame(ChatController appController)
 	{
 		super();
 		this.appController = appController;
 		this.appPanel = new ChatPanel(appController);
-		//-------------------------------------------------------------
+		
 		setupFrame();
 	}
-	//-------------------------------------------------------------
+	
 	private void setupFrame()
 	{
 		this.setContentPane(appPanel);
 		this.setTitle("Chatbot");
-		this.setSize(1000, 500);
+		this.setSize(1024, 768);
 		this.setResizable(false);
 		this.setVisible(true);
 	}
