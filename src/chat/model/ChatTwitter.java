a46444235827095d5b3648a7273d7c070a25f873
Hunter <hunterknott00@gmail.com>
2019-04-26 13:46:42 -0600

    Added all the Twitter imports

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -1,8 +1,24 @@
 package chat.model;
 
-import chat.controller.ChatController;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.text.DecimalFormat;
+
+import twitter4j.TwitterException;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.GeoLocation;
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+
+import chat.controller.ChatController;
+import chat.controller.IOController;
 
 public class ChatTwitter
 {
