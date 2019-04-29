ddc42698cac5759939107ca6dc9e5bfa4492d355
Hunter <hunterknott00@gmail.com>
2019-04-29 12:38:40 -0600

    Added createIgnoredWordArray method in ChatTwitter

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -125,4 +125,22 @@ public class ChatTwitter
 			}
 		}
 	}
+	
+	private String [] createIgnoredWordArray()
+	{
+		String [] boringWords;
+		String fileText = IOController.loadFromFile(app, "commonWords.txt");
+		int wordCount = 0;
+		
+		Scanner wordScanner = new Scanner(fileText);
+		
+		while(wordScanner.hasNextLine())
+		{
+			wordScanner.nextLine();
+			wordCount++;
+		}
+		
+		boringWords = new String[wordCount];
+		wordScanner.close();
+	}
 }
